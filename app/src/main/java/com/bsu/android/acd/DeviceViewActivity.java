464789dc5da08bf9a9ea9ec959b55106605855cf
Suraj Deuja <deujasuraj77@gmail.com>
2016-04-26 06:25:39 -0600

    moved the log view activity

--- app/src/main/java/com/bsu/android/acd/DeviceViewActivity.java
+++ app/src/main/java/com/bsu/android/acd/DeviceViewActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -172,6 +174,31 @@ public class DeviceViewActivity extends AppCompatActivity implements RpcCallback
             mDataSet = new ArrayList<>();
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.log) {
+            Intent intent = new Intent(this, LogViewActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
